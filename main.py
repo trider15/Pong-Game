db0fee0d42cf119c9cb254e9d8844b32975a42ef
Thomas Rider <102703826+trider15@users.noreply.github.com>
2022-11-05 17:56:10 -0400

    Pong Game
    interactive pong game.. using python classes and initializing objects.

--- main.py
+++ main.py
@@ -1,25 +1,51 @@
-from bs4 import BeautifulSoup
-import requests
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
 
-response = requests.get(URL)
-movie_web_page = response.text
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move_ball()
+
+    # Detect collison with ceiling and floor then bounce
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collison with each paddle then bounce
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() > -320:
+        ball.bounce_x()
+
+    #Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_score()
 
 
-soup = BeautifulSoup(movie_web_page, "html.parser")
+    # Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_score()
 
-movies = soup.find_all(name="h3", class_="title")
 
-movie_rank = []
-for item in movies:
-    movie = item.getText()
-    movie_rank.append(movie)
-
-ascendingMovieRanking = movie_rank[::-1]
-print(ascendingMovieRanking)
-
-with open("movies.txt", mode="w") as file:
-    for title in ascendingMovieRanking:
-        file.write(f"{title}\n")
-
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("white")
+        self.hideturtle()
+        self.left_score = 0
+        self.right_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.left_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.right_score, align="center", font=("Courier", 80, "normal"))
+
+    def l_score(self):
+        self.left_score += 1
+        self.update_scoreboard()
+
+    def r_score(self):
+        self.right_score += 1
+        self.update_scoreboard()
